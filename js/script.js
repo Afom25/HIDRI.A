4234a1575a011e1d6296330ae736ba0ae2f5ae81
hidri <agthidri2400@gmail.com>
2020-04-11 14:40:53 +0300

    this commit will add the style of the welcome section

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+$ (function(){
+    new WOW().init();   
+});
+$(window).on('load',function(){
+    $("#").addClass("animated fadeInDown");
+    $("#").addClass("animated fadeInRight");
+    $("#").addClass("animated zoomIn");
+    $("#").addClass("animated fadeDown infinite");
+
+});
