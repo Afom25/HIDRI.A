68c635189548119df6f8e593bc7c0f9df1bc0168
hidri <agthidri2400@gmail.com>
2020-04-11 18:58:02 +0300

    this commit will add readme.md

--- js/script.js
+++ js/script.js
@@ -11,6 +11,72 @@ $(document).ready(function(){
         $("#project-img1").show();
         $("#detail1").hide();
     })
+    /** det2 */
+    $("#project-img2").click(function(){
+        $("#detail2").show();
+        $("#project-img2").hide();
+    })
+    $("#detail2").click(function(){
+        $("#project-img2").show();
+        $("#detail2").hide();
+    })
+    $("#project-img3").click(function(){
+        $("#detail3").show();
+        $("#project-img3").hide();
+    })
+    $("#detail3").click(function(){
+        $("#project-img3").show();
+        $("#detail3").hide();
+    })
+    $("#project-img1").click(function(){
+        $("#detail1").show();
+        $("#project-img1").hide();
+    })
+    $("#detail1").click(function(){
+        $("#project-img1").show();
+        $("#detail1").hide();
+    })
+    $("#project-img1").click(function(){
+        $("#detail1").show();
+        $("#project-img1").hide();
+    })
+    $("#detail1").click(function(){
+        $("#project-img1").show();
+        $("#detail1").hide();
+    })
+    $("#project-img1").click(function(){
+        $("#detail1").show();
+        $("#project-img1").hide();
+    })
+    $("#detail1").click(function(){
+        $("#project-img1").show();
+        $("#detail1").hide();
+    })
+    $("#project-img1").click(function(){
+        $("#detail1").show();
+        $("#project-img1").hide();
+    })
+    $("#detail1").click(function(){
+        $("#project-img1").show();
+        $("#detail1").hide();
+    })
+    $("#project-img1").click(function(){
+        $("#detail1").show();
+        $("#project-img1").hide();
+    })
+    $("#detail1").click(function(){
+        $("#project-img1").show();
+        $("#detail1").hide();
+    })
+    $("#project-img1").click(function(){
+        $("#detail1").show();
+        $("#project-img1").hide();
+    })
+    $("#detail1").click(function(){
+        $("#project-img1").show();
+        $("#detail1").hide();
+    })
+    
 
 });
 
